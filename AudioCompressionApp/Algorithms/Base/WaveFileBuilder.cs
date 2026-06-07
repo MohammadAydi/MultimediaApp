b46a9194b5ca9d2466c51e53aa712b07ad6f41c7
Mohamad Alaa Alshahrour <alaaalshahror@gmail.com>
2026-06-07 12:56:02 +0300

    feat: enhance WaveFileBuilder to support 32-bit PCM output

--- AudioCompressionApp/Algorithms/Base/WaveFileBuilder.cs
+++ AudioCompressionApp/Algorithms/Base/WaveFileBuilder.cs
@@ -2,7 +2,7 @@ using System.IO;
 using AudioCompressionApp.Models;
 using NAudio.Wave;
 
-namespace AudioCompressionApp.Algorithms;
+namespace AudioCompressionApp.Algorithms.Base;
 
 public static class WaveFileBuilder {
     public static byte[] CreateWaveFile(DecompressionResult result) {
@@ -10,19 +10,24 @@ public static class WaveFileBuilder {
 
         WaveFormat format = new WaveFormat(result.SampleRate, result.BitsPerSample, result.Channels);
         using (WaveFileWriter writer = new WaveFileWriter(stream, format)) {
-            byte[] pcmBytes = new byte[result.Samples.Length * 2];
-
-            Buffer.BlockCopy(
-                result.Samples,
-                0,
-                pcmBytes,
-                0,
-                pcmBytes.Length);
-
-            writer.Write(
-                pcmBytes,
-                0,
-                pcmBytes.Length);
+            if (result.BitsPerSample == 32) {
+                // Expand short samples back to 32-bit PCM
+                byte[] pcmBytes = new byte[result.Samples.Length * 4];
+                for (int i = 0; i < result.Samples.Length; i++) {
+                    int val = result.Samples[i] << 16;
+                    pcmBytes[i * 4]     = (byte)(val & 0xFF);
+                    pcmBytes[i * 4 + 1] = (byte)((val >> 8) & 0xFF);
+                    pcmBytes[i * 4 + 2] = (byte)((val >> 16) & 0xFF);
+                    pcmBytes[i * 4 + 3] = (byte)((val >> 24) & 0xFF);
+                }
+                writer.Write(pcmBytes, 0, pcmBytes.Length);
+            }
+            else {
+                // 16-bit PCM (default)
+                byte[] pcmBytes = new byte[result.Samples.Length * 2];
+                Buffer.BlockCopy(result.Samples, 0, pcmBytes, 0, pcmBytes.Length);
+                writer.Write(pcmBytes, 0, pcmBytes.Length);
+            }
         }
         return stream.ToArray();
     }
